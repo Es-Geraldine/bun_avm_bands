7430a585dd8d08467c8b79aa4114e33e8ef1bc98
alumno <alumno@gmail.com>
2026-05-12 15:59:08 +0200

    grivrod: Creacion Store y el Service

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,12 @@ export const creareServer = () => {
     socket.on("chat", (msg)=> io.emit("chat", msg));
 })
 
+io.on("disconnect", (socket) => {
+    console.log(`Cliente desconectado: ${socket.id}`);
+})
+
+
+
 
   const { fetch: engineFetch, websocket } = engine.handler();
 
